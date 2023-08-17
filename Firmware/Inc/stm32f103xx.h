21c100e7eb6585ce20b8d2be3f037d56067d2916
Lalit Kumar <lalit.k.saroya@gmail.com>
2023-08-16 22:39:07 -0600

    Update stm32f103xx.h

--- Firmware/Inc/stm32f103xx.h
+++ Firmware/Inc/stm32f103xx.h
@@ -1,8 +1,18 @@
 /**
  * @file stm32f103xx.h
- * @author Lalit Kumar (Lalitk.space)
+ * @author Lalit Kumar (www.lalitk.space)
  * @brief This file Contains all the details specific to STM32F103xx microcontroller.
  * @version 1.0
  * @date 2023-08-15
  * 
  */
+
+#ifndef STM32F103XX_H_
+#define STM32F103XX_H_
+
+
+
+
+
+
+#endif /* STM32F103XX_H_ */
